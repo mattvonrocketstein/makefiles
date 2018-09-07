13491b85d11433839ace916330ca4483ad9ec5cb
Pawel Hajduk <phajduk.evojam@605.tv>
2018-09-07 15:01:29 +0200

    INFRA-642 Added cross account ECR replication

--- Makefile.aws-ecr.mk
+++ Makefile.aws-ecr.mk
@@ -48,11 +48,15 @@ ecr-create-repo: require-jq assert-ECR_PROJECT assert-AWS_REGION
 #
 ecr-mirror: ecr-login assert-DOCKER_TAG assert-DOCKER_IMAGE assert-DOCKER_REGISTRY assert-ECR_BASE assert-ECR_REPO
 	$(call _announce_target, $@)
+	ECR_PROJECT=$(value ECR_REPO) ${MAKE} ${MY_MAKEFLAGS} ecr-create-repo
+	${MAKE} ${MY_MAKEFLAGS} ecr-mirror-do
+
+ecr-mirror-do: assert-DOCKER_TAG assert-DOCKER_IMAGE assert-DOCKER_REGISTRY assert-ECR_BASE assert-ECR_REPO
+	$(call _announce_target, $@)
 	docker pull $(value DOCKER_REGISTRY)/$(value DOCKER_IMAGE):$(value DOCKER_TAG)
 	docker tag \
 	$(value DOCKER_REGISTRY)/$(value DOCKER_IMAGE):$(value DOCKER_TAG) \
 	$(value ECR_BASE)/$(value ECR_REPO):$(value DOCKER_TAG)
-	ECR_PROJECT=$(value ECR_REPO) ${MAKE} ${MY_MAKEFLAGS} ecr-create-repo
 	docker push $(value ECR_BASE)/$(value ECR_REPO):$(value DOCKER_TAG)
 
 # example usage:
@@ -61,3 +65,36 @@ ecr-push: ecr-login assert-DOCKER_TAG assert-ECR_BASE assert-ECR_REPO
 	$(eval DEST_TAG?=${DOCKER_TAG})
 	docker tag $(value DOCKER_TAG) $(value ECR_BASE)/$(value ECR_REPO)/$(value DEST_TAG)
 	docker push $(value ECR_BASE)/$(value ECR_REPO)/$(value DEST_TAG)
+
+# example
+# make ecr-mirror-all-x-account \
+# ECR_REPO_FILTER=605data \
+# SRC_AWS_REGION=us-east-1 SRC_ECR_BASE=248783370565.dkr.ecr.us-east-1.amazonaws.com SRC_AWS_PROFILE=605-legacy \
+# DST_AWS_REGION=us-east-2 DST_ECR_BASE=873326152210.dkr.ecr.us-east-2.amazonaws.com DST_AWS_PROFILE=605-management
+ecr-mirror-all-x-account: require-jq assert-ECR_REPO_FILTER assert-SRC_AWS_REGION assert-SRC_ECR_BASE assert-SRC_AWS_PROFILE assert-DST_AWS_REGION assert-DST_ECR_BASE assert-DST_AWS_PROFILE
+	@$$(aws ecr get-login --no-include-email --region ${SRC_AWS_REGION} --profile ${SRC_AWS_PROFILE})
+	@$$(aws ecr get-login --no-include-email --region ${DST_AWS_REGION} --profile ${DST_AWS_PROFILE})
+	aws ecr describe-repositories --region ${SRC_AWS_REGION} --profile ${SRC_AWS_PROFILE} | \
+	jq -r ".repositories[].repositoryName" | \
+	grep ${ECR_REPO_FILTER} \
+	> ecr-repos.tmp
+	xargs -0 -I '{}' -n 1 ${MAKE} ecr-mirror-x-account ${MAKEFLAGS} ECR_REPO='{}' < <(tr \\n \\0 <ecr-repos.tmp)
+	rm ecr-repos.tmp
+
+# example
+# make ecr-mirror-x-account \
+# ECR_REPO=605data/chatops \
+# SRC_AWS_REGION=us-east-1 SRC_ECR_BASE=248783370565.dkr.ecr.us-east-1.amazonaws.com SRC_AWS_PROFILE=605-legacy \
+# DST_AWS_REGION=us-east-2 DST_ECR_BASE=873326152210.dkr.ecr.us-east-2.amazonaws.com DST_AWS_PROFILE=605-management
+ecr-mirror-x-account: require-jq assert-ECR_REPO assert-SRC_AWS_REGION assert-SRC_ECR_BASE assert-SRC_AWS_PROFILE assert-DST_AWS_REGION assert-DST_ECR_BASE assert-DST_AWS_PROFILE
+	$(call _announce_target, $@)
+	@$$(aws ecr get-login --no-include-email --region ${SRC_AWS_REGION} --profile ${SRC_AWS_PROFILE})
+	@$$(aws ecr get-login --no-include-email --region ${DST_AWS_REGION} --profile ${DST_AWS_PROFILE})
+	aws ecr describe-images --region ${SRC_AWS_REGION} --profile ${SRC_AWS_PROFILE} --repository-name ${ECR_REPO} | \
+	jq -r '.imageDetails[].imageTags[]' \
+	> ecr-image-tags.tmp
+	AWS_REGION=$(value DST_AWS_REGION) AWS_PROFILE=$(value DST_AWS_PROFILE) ECR_PROJECT=$(value ECR_REPO) ${MAKE} ecr-create-repo
+	DOCKER_IMAGE=$(value ECR_REPO) \
+	DOCKER_REGISTRY=$(value SRC_ECR_BASE) ECR_BASE=$(value DST_ECR_BASE) \
+	xargs -0 -I '{}' -n 1 ${MAKE} ecr-mirror-do DOCKER_TAG='{}' < <(tr \\n \\0 <ecr-image-tags.tmp)
+	rm ecr-image-tags.tmp
