0a84b534b84eb53e8f7889e768403ab17007b87a
mvr <matt@example.com>
2018-12-03 19:14:13 -0500

    tagging

--- Makefile.packer.mk
+++ Makefile.packer.mk
@@ -9,7 +9,9 @@ PACKER_CONFIG_YAML ?= packer.yaml
 PACKER_CONFIG_JSON ?= packer.json
 PACKER_CONFIG = ${PACKER_CONFIG_JSON}
 PACKER_KEY_FILE ?= packer.pem
-export PACKER_CONFIG PACKER_KEY_FILE PACKER_CONFIG_YAML PACKER_CONFIG_JSON PACKER_MANIFEST
+PACKER_IMAGE ?= 870326185936.dkr.ecr.us-east-2.amazonaws.com/605data/packer:605
+export PACKER_IMAGE PACKER_MANIFEST
+export PACKER_CONFIG PACKER_KEY_FILE PACKER_CONFIG_YAML PACKER_CONFIG_JSON
 
 PACKER_REPO:=$(shell basename -s .git `git config --get remote.origin.url`)
 PACKER_AMI_NAME:=$(shell python -c "print '${PACKER_REPO}'.replace('ami-', '').replace('ami','')")
@@ -45,10 +47,38 @@ packer-get-key:
 	> ${SRC_ROOT}/$(value PACKER_KEY_FILE)
 	chmod go-rwx ${SRC_ROOT}/$(value PACKER_KEY_FILE)
 
+# this is horrible, but there's no time to cleanup.
+# this script should be baked into our packer docker
+define PY_PACKER_TAGGER
+import os
+import json
+packer_manifest = os.environ["PACKER_MANIFEST"]
+packer_config = os.environ["PACKER_CONFIG_JSON"]
+packer_tag = os.environ["PACKER_TAG"]
+print "parsing packer manifest at: {}".format(packer_manifest)
+assert os.path.exists(packer_manifest), "manifest does not exist!"
+manifest = json.loads(open(packer_manifest).read())
+print "parsing packer config at: {}".format(packer_config)
+assert os.path.exists(packer_config), "config does not exist!"
+config = json.loads(open(packer_config).read())
+profile = config["builders"][0]["profile"]
+artifacts = [b["artifact_id"] for b in manifest["builds"]]
+cmd_t = "echo AWS_DEFAULT_REGION={region} AWS_PROFILE={profile} aws ec2 create-tags --resources {ami} --tags Key={tag},Value=True"
+artifacts = [artifact.split(":") for artifact in artifacts]
+cmds = [cmd_t.format(region=region, profile=profile, ami=packer_ami,
+                     tag=packer_tag,) for region, packer_ami in artifacts]
+for cmd in cmds:
+    print "Executing: {}".format(cmd)
+    os.system(cmd)
+endef
+packer-tag: assert-PACKER_TAG assert-PACKER_MANIFEST
+	@printf '$(subst $(newline),\n,${PY_PACKER_TAGGER})'|python /dev/stdin
+
 packer-build: assert-PACKER_IMAGE assert-PACKER_CONFIG packer-get-key
 	$(call _announce_target, $@)
 	cat ${PACKER_CONFIG} | jq . \
-	&& docker run -i \
+	&& env | grep PACKER_ | docker run -i \
+	--env-file /dev/stdin \
 	-v `pwd`:/workspace \
 	-v ~/.aws:/root/.aws \
 	-w /workspace \
